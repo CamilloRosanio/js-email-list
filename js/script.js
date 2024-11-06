b2e594d579874317b05c8cc92a06fd45ecd58cbe
MyUsername <rosanio.c@gmail.com>
2024-11-06 13:44:34 +0100

    HTML structure

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-// API BOOLEAN
-// https://flynn.boolean.careers/exercises/api/random/mail
+/**********************************************************
+| # CONSEGNA
+*********************************************************/
+
+/*
+Descrizione
+Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Abbellire con CSS o Bootstrap
+Inserire un bottone che al click faccia il fetch per altre 10 mail (sostituendo le altre)
+*/
 
 
+
+/**********************************************************
+| # SVOLGIMENTO
+*********************************************************/
